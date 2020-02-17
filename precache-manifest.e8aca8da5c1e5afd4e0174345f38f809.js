e5f4f27d9d1475d7e3260faf5177825c4cb2f1c7
ShivnathTelemerge <shivnath.s@itstelemerge.com>
2020-02-17 16:12:31 +0530

    Updates

---
+++ precache-manifest.e8aca8da5c1e5afd4e0174345f38f809.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d58b214a5a134838258506e9bb1727",
+    "url": "/Chatbot/index.html"
+  },
+  {
+    "revision": "a681f9ac6c583a2d85df",
+    "url": "/Chatbot/static/css/2.5e8967e3.chunk.css"
+  },
+  {
+    "revision": "f40f2840a56337a73301",
+    "url": "/Chatbot/static/css/main.05e00092.chunk.css"
+  },
+  {
+    "revision": "a681f9ac6c583a2d85df",
+    "url": "/Chatbot/static/js/2.bc501e30.chunk.js"
+  },
+  {
+    "revision": "76c775842385d8ffed40f4c86b2b4641",
+    "url": "/Chatbot/static/js/2.bc501e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f40f2840a56337a73301",
+    "url": "/Chatbot/static/js/main.bb5e8f22.chunk.js"
+  },
+  {
+    "revision": "8755b5f1996f6dd035b2",
+    "url": "/Chatbot/static/js/runtime-main.9ae86c47.js"
+  },
+  {
+    "revision": "bbb1449c18085be9e509f1c5c6701883",
+    "url": "/Chatbot/static/media/image4.bbb1449c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Chatbot/static/media/logo.5d5d9eef.svg"
+  }
+]);
